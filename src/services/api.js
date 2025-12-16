e066e716b5f9b780ee651598445e45d89a6fd052
Mebrie-Awoke <mebrieawoke941@gmail.com>
2025-12-16 12:00:00 +0300

    Update API service configuration

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
   timeout: 10000,
 });
-
+ 
 api.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem('token');
   if (token) {
@@ -22,4 +22,3 @@ export const submitAppointment = async (payload) => {
     return { error: true, message: error.message };
   }
 }; 
-   
