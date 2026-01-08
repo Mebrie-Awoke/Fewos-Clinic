5aebc5485a2483a150014cdc6e3bfb01692a6fb8
Mebrie-Awoke <mebrieawoke941@gmail.com>
2026-01-08 12:00:00 +0300

    Update home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -31,7 +31,6 @@ export default function HomeScreen() {
   if (loading) {
     return <Loading />;
   }
-  
   return (
     <SafeAreaView style={styles.safeArea} edges={['top', 'bottom']}>
       <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
